24a61e08314560368f9133213360fc9afb21756d
SyedAhmed-Coder <syedahmed.w@gmail.com>
2023-02-07 14:09:26 +0500

    this dev push 2.0

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "This the devlopment branch pushing Version 1.0.",
+        "message": "This the devlopment branch pushing Version 2.0.",
         "input": event,
     }
 
